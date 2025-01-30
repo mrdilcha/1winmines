a69cde1b1abadb007bdf95783ee38a94e4a8889d
mrdilcha <creddyp4@gmail.com>
2025-01-31 00:30:10 +0530

    Delete src/app directory

--- src/app/_app.tsx
+++
@@ -1,8 +0,0 @@
-// pages/_app.tsx
-import '../styles/global.css';  // Import global CSS
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
